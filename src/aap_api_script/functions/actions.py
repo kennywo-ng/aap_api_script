d678f23fe83b28e387863f1071defd3c68a23b08
kenny <kennywong@kenny>
2026-04-21 17:01:23 +0800

    test

---
+++ src/aap_api_script/functions/actions.py
@@ -0,0 +1,128 @@
+from api.client import APIClient
+from config.settings import (
+    API_BASE_URL,
+    API_TOKEN,
+    API_TIMEOUT,
+    AUTH_PASSWORD,
+    AUTH_USERNAME,
+    ENV,
+)
+
+from . helpers import (
+    get_host_w_inventory,
+    excel_parse_hostname,
+    match_hosts,
+    sel_id
+)
+from api import (
+    get_inv,
+    get_inv_group,
+    post_bulk_imp,
+    post_grp_host
+)
+
+if ENV == "prod":
+    client = APIClient(
+        base_url=API_BASE_URL,
+        username=AUTH_USERNAME,
+        password=AUTH_PASSWORD,
+        timeout=API_TIMEOUT
+    )
+elif ENV == "stage":
+    client = APIClient(
+        base_url=API_BASE_URL,
+        token=API_TOKEN,
+        timeout=API_TIMEOUT
+    )
+
+import sys
+
+def func_1():
+    host_w_inv = get_host_w_inventory(client)
+    for item in host_w_inv:
+        print(f"Host: {item['host_name']:<40} Inventory: {item['inventory_name']}")
+
+def func_2():
+    exc_host = excel_parse_hostname('excels/host_check.xlsx')
+    host_w_inv = get_host_w_inventory(client)
+    matches = match_hosts(exc_host, host_w_inv)
+
+    for item in sorted(matches, key=lambda x: x['inventory_name']):
+        print(f"Host: {item['host_name']:<40} Inventory: {item['inventory_name']}")
+
+def func_3():
+    excel_bulk_host = excel_parse_hostname('excels/host_bulk_import.xlsx')
+    aap_inv = get_inv(client)
+    selected_inv = None
+
+    while selected_inv is None:
+        selected_inv = sel_id(aap_inv, "inventory")
+
+        if not selected_inv:
+            print("\nInventory ID not found. Please try again.")
+        
+        print("\nWould you like to add the following hosts to: " + selected_inv['name'])
+
+    for host in excel_bulk_host:
+        print(f"- {host['Hostname']}")
+
+    while True:
+        confirm = input("\nConfirm? (y/n): ").strip().lower()
+
+        if confirm == 'y':
+            break
+        elif confirm == 'n':
+            sys.exit("Operation cancelled.")
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
+
+    import_bulk = post_bulk_imp(client, selected_inv['id'], excel_bulk_host)
+
+    if import_bulk.status_code in range(200, 300):
+        print("Status code: " + str(import_bulk.status_code))
+    else:
+        print(f"Failed to import hosts. {import_bulk.json()['__all__']}")
+
+def func_4():
+    exc_group_host = excel_parse_hostname('excels/host_grouping.xlsx')
+    aap_inv = get_inv(client)
+    selected_inv = None
+    selected_grp = None
+
+    while selected_inv is None:
+        selected_inv = sel_id(aap_inv, "inventory")
+        if selected_inv:
+            break
+        else:
+            print("\nInventory ID not found. Please try again.")
+
+    aap_inv_groups = get_inv_group(client, selected_inv['id'])
+
+    while selected_grp is None:
+        selected_grp = sel_id(aap_inv_groups, "group")
+        if selected_grp:
+            break
+        else:
+            print("\nGroup ID not found. Please try again.")
+
+    print(f"\nInventory: {selected_inv['name']} \nGroup: {selected_grp['name']} ")
+    print("Hosts to be added to group:")
+    for host in exc_group_host:
+        print(f"- {host['Hostname']}")
+
+    confirm = input("\nConfirm? (y/n): ").strip().lower()
+
+    if confirm not in ('y', 'n'):
+        print("Invalid input. Please enter 'y' or 'n'.")
+        return
+
+    if confirm == 'n':
+        print("Cancelled.")
+        return
+    
+    for host in exc_group_host:
+        resp = post_grp_host(client, selected_grp['id'], host['Hostname'])
+        if resp.status_code in range(200, 300):
+            print(f"Added host {host['Hostname']} to group {selected_grp['name']}.")
+        else:
+            print(f"Failed to add host {host['Hostname']}. {resp.json()}")
---
+++ src/aap_api_script/functions/helpers.py
@@ -0,0 +1,52 @@
+import pandas
+import json
+from config.settings import (
+    DOMAIN_NAME
+)
+
+def match_hosts(excel_list, aap_inv_list):
+    lookup = {item["host_name"]: item["inventory_name"] for item in aap_inv_list}
+    
+    result = []
+    for item in excel_list:
+        host = item['Hostname']
+        if host in lookup:
+            result.append({
+                "host_name": host,
+                "inventory_name": lookup[host]
+                })
+        else:
+            result.append({
+                "host_name": host,
+                "inventory_name": "Not Found"
+                })
+    
+    return result
+
+def excel_parse_hostname(file_path: str) -> dict:
+    excel = pandas.read_excel(file_path, engine='openpyxl').to_json(orient='records')
+    excel = json.loads(excel)
+    for item in excel:
+        if DOMAIN_NAME not in item['Hostname']:
+            item['Hostname'] = f"{item['Hostname']}.{DOMAIN_NAME}"
+    return excel
+
+def sel_id(inv: list, type: str) -> list | None:
+    print(
+        f"\nWhich {type} to add hosts to?\n"
+        f"{'ID':<5}{type} Name"
+    )
+
+    for item in inv:
+        print(f"{item['id']:<5}{item['name']}")
+
+    inv_id = None
+    while inv_id is None:
+        try:
+            inv_id = int(input(f"\nEnter {type} ID: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid ID.")
+
+    inv_id_match = next((i for i in inv if i['id'] == inv_id), None)
+
+    return inv_id_match
